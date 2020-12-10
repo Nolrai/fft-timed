ae0f5e7c7785f959911d28e328363bbfe4468158
Nolrai <chrisaupshaw@gmail.com>
2020-12-09 23:53:45 -0800

    Runs naive ft.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,56 @@
-module Main (main) where
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import FftTimed (projectName)
+module Main
+  ( main,
+  )
+where
 
+import Codec.Wav as W
+import Control.DeepSeq
+import Data.Array.Unboxed as A
+import Data.Audio
+import Data.Complex (Complex ((:+)), Complex, magnitude)
+import Data.Int
+import Data.Vector.Unboxed as V
+import FftTimed (naiveFT, projectName)
+import System.Environment
+import System.IO (IOMode (WriteMode), hPrint, openFile)
 
 main :: IO ()
-main = putStrLn ("Executable for " ++ projectName)
+main =
+  do
+    putStrLn (projectName <> ":")
+    [inFileName, outFileName] <- getArgs
+    result <- W.importFile inFileName
+    case result of
+      Left err -> putStrLn err
+      Right Audio {..} ->
+        do
+          putStr "sampleRate: "
+          print sampleRate
+          putStr "channelNumber: "
+          print channelNumber
+          putStr "bounds: "
+          print $ bounds sampleData
+          let ft = body channelNumber sampleData
+          outHandle <- ft `deepseq` openFile outFileName WriteMode
+          (hPrint outHandle . magnitude) `V.mapM_` ft
+          putStrLn $ "Wrote File: " <> outFileName
+
+body :: Int -> UArray Int Int16 -> Vector (Complex Double)
+body channelNumber sampleData = naiveFT . V.take 1000 $ (:+ 0) `V.map` scaledVector
+  where
+    start :: Int
+    end :: Int
+    arraySize :: Int
+    (start, end) = A.bounds (sampleData :: UArray Int Int16)
+    arraySize = (end - start) `div` channelNumber
+    getSample :: Int -> Int16
+    getSample ix = sampleData A.! (ix * channelNumber + start)
+    rawVector :: Vector Int16
+    rawVector = V.generate (arraySize - 1) getSample
+    max' = V.maximum rawVector
+    min' = V.minimum rawVector
+    scaledVector :: Vector Double
+    scaledVector = (\x -> fromIntegral (x - min') / (fromIntegral (max' - min') * fromIntegral arraySize)) `V.map` rawVector
--- src/FftTimed.hs
+++ src/FftTimed.hs
@@ -1,15 +1,27 @@
-{- |
-Copyright: (c) 2020 Chris Upshaw
-SPDX-License-Identifier: MIT
-Maintainer: Chris Upshaw <chrisaupshaw@gmail.com>
-
-See README for more info
--}
-
+-- |
+-- Copyright: (c) 2020 Chris Upshaw
+-- SPDX-License-Identifier: MIT
+-- Maintainer: Chris Upshaw <chrisaupshaw@gmail.com>
+--
+-- See README for more info
 module FftTimed
-    ( projectName
-    ) where
+  ( projectName,
+    naiveFT,
+  )
+where
 
+import Data.Complex as C
+import Data.Vector.Unboxed as V
 
 projectName :: String
 projectName = "fft-timed"
+
+zetaVect :: Int -> Vector (Complex Double)
+zetaVect bigN = V.generate bigN $ \ix -> cis ((2 * pi * fromIntegral ix) / fromIntegral bigN)
+
+naiveFT :: Vector (Complex Double) -> Vector (Complex Double)
+naiveFT v = generate bigN $ \ix -> V.sum $ V.imap (mkEntry ix) v
+  where
+    mkEntry ix ix' x = x * (zetaVect' ! (ix * ix' `rem` bigN))
+    bigN = V.length v
+    zetaVect' = zetaVect bigN
